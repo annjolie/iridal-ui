60e5cccc9522db4f6bc6a0b09a8d483918baa5a4
annjolie <annjolie.lourido@gmail.com>
2023-03-10 09:16:16 +0100

    cleaning up

--- src/components/Input/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Container } from './styles';
-import React from 'react';
-import type { InputProps } from './types';
-
-export default function Input({ className }: InputProps): JSX.Element {
-  return <Container className={className}>asd</Container>;
-}
--- src/components/Input/types.ts
+++
@@ -1,1 +0,0 @@
-export type InputProps = { className?: string };
