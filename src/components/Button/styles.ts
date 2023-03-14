78fee0024c8f164d2c8a836b529df1a33edab75b
annjolie <annjolie.lourido@gmail.com>
2023-03-14 12:51:55 +0100

    button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.button`
-  font-family: ${({ theme }) => theme.fonts.monserrat};
+  font-family: ${({ theme }) => theme.fonts.lato};
   font-size: ${({ theme }) => theme.fontSizes.xl};
 `;
 
